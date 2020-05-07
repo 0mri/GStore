394fa6d67338664a3beb7a2c409c04f876f7401d
Omri Efraim <omri@efra.im>
2020-05-07 11:00:40 +0300

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 module.exports = {
   outputDir: 'dist',
   assetsDir: 'static',
-  publicPath: IS_PRODUCTION ? 'https://g-store-app.herokuapp.com/' : '/',
+  // publicPath: IS_PRODUCTION ? 'https://g-store-app.herokuapp.com/' : '/',
   // publicPath: 'http://localhost:8000/',
   // For Production, replace set baseUrl to CDN
   // And set the CDN origin to `yourdomain.com/static`
